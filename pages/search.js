b743f54e4e38e7ae1efb25a9a19ed584f12cff19
Nikhil Gupta <nikgupta06@gmail.com>
2022-02-13 13:28:42 -0600

    Debounce song search API call

--- pages/search.js
+++ pages/search.js
@@ -3,20 +3,49 @@ import Head from 'next/head'
 import React from 'react';
 import { Input } from '@nextui-org/react';
 import { Button } from '@nextui-org/react';
+import axios from 'axios';
 
-
+var timeout = null;
 class Search extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { results: null };
+        this.state = {
+            query: null,
+            results: null,
+            timeout: null,
+        };
     }
 
-    // Auto-binding functions
-    handleClick = (ev) => {
-        console.log('??? - ' + this.state.results)
+    textTyped = (event) => {
+        // Update component state
+        console.log('Updating state...')
         this.setState((state, props) => ({
-            results: [1, 2, 3, 4]
+            query: event.target.value
+        }));
+        // Debounce the API call
+        this.debounce(this.getSearchResults, 1000, false)();
+    }
+
+    debounce = (func, wait, immediate) => {
+        console.log(`Debouncing with wait=${wait} and immediate=${immediate}`)
+        return function () {
+            var context = this, args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+
+    getSearchResults = async () => {
+        let newResults = await axios.get(`http://localhost:8100/search/sp/${this.state.query}`);
+        this.setState((state, props) => ({
+            results: newResults.data
         }));
     }
 
@@ -29,25 +58,25 @@ class Search extends React.Component {
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
 
-                {/* Center content horizontally/vertically on the screen */}
                 <div className={styles.main}>
-                    {/* Main box where the search query will be entered */}
                     <Input size="xl" labelPlaceholder="Search for a song..."
                         shadow={false} type="search" bordered color="primary"
-                        onChange={this.handleClick} />
-                    {/* Only show if results are present */}
+                        onChange={this.textTyped} />
                     {this.state.results != null && <Button.Group size="xl" vertical bordered>
                         {
-                            // Create a button for each result
                             this.state.results.map((result, idx) => (
                                 <Button>Button #{idx}</Button>
                             ))
                         }
                     </Button.Group>}
+                    <pre>
+                        {JSON.stringify(this.state.results, null, 2)}
+                    </pre>
                 </div>
 
             </div>
         )
     }
 }
+
 export default Search;
